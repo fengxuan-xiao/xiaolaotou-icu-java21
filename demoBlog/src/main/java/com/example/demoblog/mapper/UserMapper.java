84b5d525404e53d15f53be9865c5910507b21711
xiao <1418618784@qq.com>
2026-04-29 00:31:50 +0800

    20260429

--- demoBlog/src/main/java/com/example/demoblog/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoblog.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demoblog.entity.Users;
-
-public interface UserMapper extends BaseMapper<Users> {
-}
